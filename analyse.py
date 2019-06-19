bc302c7a8444b77c1472f85a750e2a9e34590360
wtkomc <egor.v.stepanov@gmail.com>
2019-06-19 03:05:36 +0100

    2019-06-19

--- analyse.py
+++ analyse.py
@@ -1,7 +1,10 @@
+import pandas as pd
 import networkx as nx
+import numpy as np
 import matplotlib.pyplot as plt
-import itertools
 from collections import defaultdict
+import itertools
+import random
 import tools
 import re
 
@@ -74,8 +77,9 @@ def standardise_name(name):
 def preprocess():
     global person_id2name, person_name2id
     global office_id2name, office_name2id
+    global office2persons, person2offices
 
-    need_update = True
+    need_update = False
 
     person_id2name = tools.get_mapping('person', 'person', 'id', 'name',
                                        update=need_update)
@@ -86,15 +90,92 @@ def preprocess():
     office_name2id = tools.get_mapping('office', 'office', 'name', 'id', as_set=True,
                                        update=need_update)
 
+    office2persons = tools.get_mapping('office', 'person', 'id', 'id', as_set=True,
+                                       update=need_update)
+    person2offices = tools.get_mapping('person', 'office', 'id', 'id', as_set=True,
+                                       update=need_update)
+
+
+def area(values):
+    total = 0
+    for i in range(1, len(values)):
+        total += abs(values[i] - values[i-1])
+    return total * 0.5
+
+
+def income_analysis():
+    global income_data
+
+    json_data = tools.get_json()
+    data = []
+    for entry in json_data:
+        year = entry['main']['year']
+        person_id = entry['main']['person']['id']
+        office_id = entry['main']['office']['id']
+        income = sum([e['size']
+                      for e in entry['incomes']])
+
+        data.append([year, person_id, office_id, income])
+
+    dframe = pd.DataFrame(data)
+    dframe.columns = ['year', 'person_id', 'office_id', 'income']
+
+    office_id = office_name2id['Государственная Дума'][0]
+    print('office_id:', office_id)
+    dframe = dframe.loc[dframe.loc[:, 'office_id'] == int(office_id), :]
+
+    prs = []
+    income_data = defaultdict(defaultdict)
+    for p in office2persons[str(office_id)]:
+        entries = dframe.loc[dframe.loc[:, 'person_id'] == int(p), :]
+        values = []
+        for year in range(2010, 2018+1):
+            this_year = entries.loc[entries.loc[:, 'year'] == year, :]
+            if this_year.shape[0] > 0:
+                values.append(this_year.at[this_year.index[0], 'income'])
+            else:
+                values.append(None)
+        income_data[p] = pd.Series(values, index=range(2010, 2018+1))
+        if not None in values:
+            prs.append(p)
+
+    print('Total selected:', len(prs))
+    # for p in prs:
+    #    for q in prs:
+    #        if p != q:
+    # values_p = income_data[p].values.copy()
+    # values_q = income_data[q].values.copy()
+    # area_p = area(values_p)
+    # area_q = area(values_q)
+
+    # for i in range(len(values_p)):
+    #    values_p[i] /= area_p
+    #    values_q[i] /= area_q
+
+    # corr = np.corrcoef(values_p,
+    #                   values_q)[0, 1]
+    #            corr = np.corrcoef(income_data[p].values,
+    #                               income_data[q].values)[0, 1]
+
+    #            if corr > 0.9:
+    #                print(p, person_id2name[str(p)])
+    #                print(q, person_id2name[str(q)])
+    #                print(corr)
+
+    #                fig, ax = plt.subplots()
+    #                ax.plot(income_data[p].index, income_data[p].values,
+    #                        label=person_id2name[str(p)])
+    #                ax.plot(income_data[q].index, income_data[q].values,
+    #                        label=person_id2name[str(q)])
+    #                plt.legend(loc='bottom right')
+    #                plt.show()
+
 
 if __name__ == '__main__':
     preprocess()
 
-    for p in person_name2id:
-        if (len(person_name2id[p]) > 1):
-            print(p, person_name2id[p], [person_id2name[str(id)]
-                                         for id in person_name2id[p]])
-
+    income_analysis()
     # for p in person_name2id:
-    #    if 10 in person_name2id[p]:
-    #        print(p, person_name2id[p])
+    #    if (len(person_name2id[p]) > 1):
+    #        print(p, person_name2id[p], [person_id2name[str(id)]
+    #                                     for id in person_name2id[p]])
